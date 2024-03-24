382e216ea24c3bb703b4d964f86c3343c01f3f54
yazidalg <yazid.al2418@gmail.com>
2024-03-25 00:54:50 +0700

    implementasi design by contract

--- TP_MOD_6/Program.cs
+++ TP_MOD_6/Program.cs
@@ -1,9 +1,30 @@
-﻿public class MainProgram
+﻿using System.Diagnostics.Contracts;
+
+public class MainProgram
 {
     static void Main(string[] args)
     {
-        SayaTubeVideo test = new SayaTubeVideo("Tutorial Design By Contract - Yazid Al Ghozali");
-        test.PrintVideoDetails();
+        SayaTubeVideo test = null;
+        try
+        {
+            test = new SayaTubeVideo("Tutorial Design By Contract - Yazid Al Ghozali");
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                test.IncreasePlayCount(1);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+        finally
+        {
+            if (test != null)
+            {
+                test.PrintVideoDetails();
+            }
+        }
     }
 }
 
@@ -15,8 +36,10 @@ public class SayaTubeVideo
     private int playCount;
 
     Random random = new Random();
+
     public SayaTubeVideo(string title)
     {
+        Contract.Requires(title != null && title.Length <= 100, "Judul Video Memiliki Panjang Maksimal 100 Char dan tidak boleh null");
         this.title = title;
         playCount = 0;
         id = random.Next(1, 12);
@@ -24,6 +47,19 @@ public class SayaTubeVideo
 
     public void IncreasePlayCount(int count)
     {
+        Contract.Requires(count > 0 && count <= 10000000, "Input Penambahan Play Count 10000000");
+        Contract.Requires(playCount <= int.MaxValue - count, "Play Count melebihi jumlah");
+        try
+        {
+            checked
+            {
+                playCount += count;
+            }
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Error: Penambahan Play Count Melebihi Batas");
+        }
         playCount = count;
     }
 
