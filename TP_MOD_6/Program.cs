8704d76942d671ea81e435e40a786aa0992faed2
yazidalg <yazid.al2418@gmail.com>
2024-03-25 00:29:38 +0700

    add class SayaTubeVideo

--- TP_MOD_6/Program.cs
+++ TP_MOD_6/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class MainProgram
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo test = new SayaTubeVideo("Tutorial Design By Contract - Yazid Al Ghozali");
+        test.PrintVideoDetails();
+    }
+}
 
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    Random random = new Random();
+    public SayaTubeVideo(string title)
+    {
+        this.title = title;
+        playCount = 0;
+        id = random.Next(1, 12);
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        playCount = count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("ID : " + id);
+        Console.WriteLine("Title : " + title);
+        Console.WriteLine("Lagu diputar : " + playCount);
+    }
+}
